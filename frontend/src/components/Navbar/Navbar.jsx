921fa36b784976ab469452463b1dc7e8d55cb6a5
goldsnort <dheerajrajput7056@gmail.com>
2023-01-17 22:00:59 +0530

    added code page

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState, React } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// This Navbar has a functionality to include navlinks, uncomment the commented parts inside this file and css
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  return (
+    <>
+      <nav className="nav">
+        <div className="navbar-container">
+          <div className="menu-switch" onClick={handleClick}>
+            <i className="fas fa-bars fa-2x" />
+          </div>
+          <div className="navbar-left">
+            <Link to="/" className="navbar-logo">
+              Codex
+            </Link>
+
+            <div
+              className={
+                click ? "navbar-left-items active" : "navbar-left-items"
+              }
+            >
+              {/* <Link to="/">Noted</Link> */}
+            </div>
+            <div></div>
+          </div>
+          <div className="navbar-right">
+            <Link to="/" className="navbar-btn">
+              <button className="auth-button">Sign up</button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
--- frontend/src/pages/CodeExecution/CodeExecution.jsx
+++ frontend/src/pages/CodeExecution/CodeExecution.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import "./CodeExecution.css";
 
 function CodeExecution() {
-  return <div>CodeExecution</div>;
+  return (
+    <div className="executor__page">
+      <Navbar />
+      <div className="executor__container">
+        <section className="executor__code">
+          <div className="code__heading">Code.cpp</div>
+          <textarea className="code__textarea">
+            //you can enter your code here
+          </textarea>
+        </section>
+        <section className="executor__io">
+          <div className="executor__input">
+            <div className="input__heading">Input.txt</div>
+            <textarea className="input__textarea">
+              enter your input here
+            </textarea>
+          </div>
+          <div className="executor__output">
+            <div className="output__heading">Output.txt</div>
+            <textarea className="output__textarea" readOnly>
+              press run to see your output
+            </textarea>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default CodeExecution;
