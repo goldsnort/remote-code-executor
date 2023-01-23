9202795ad6f626d9354a175f3cc55df181d01591
Rithvik <imt_2020079@iiitm.ac.in>
2023-01-23 18:29:32 +0530

    language dropdown added

--- frontend/src/assets/api/api.js
+++ frontend/src/assets/api/api.js
@@ -1,13 +1,13 @@
 const baseURL = "http://localhost:4000";
 
-function handleCode(code, input) {
+function handleCode(code, input, selectedLanguage) {
   fetch(`${baseURL}/code`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     // credentials: "include",    //later uncomment it and add cors for server url later
-    body: JSON.stringify({ code: code, input: input }),
+    body: JSON.stringify({ code: code, input: input , selectedDesigner : selectedLanguage}),
   })
     .then((res) => {
       if (res.ok === true) {
--- frontend/src/pages/CodeExecution/CodeExecution.jsx
+++ frontend/src/pages/CodeExecution/CodeExecution.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import Navbar from "../../components/Navbar/Navbar";
 import "./CodeExecution.css";
 import handleCode from "../../assets/api/api";
@@ -6,12 +8,13 @@ import handleCode from "../../assets/api/api";
 function CodeExecution() {
   const [code, setCode] = useState("//you can enter your code here");
   const [input, setInput] = useState("");
+  const [selectedLanguage , setSelectedLanguage] = useState("C++");
 
   function runCode(e) {
     e.preventDefault();
-    handleCode(code, input);
+    handleCode(code, input,selectedLanguage);
   }
-
+  // const codeString = '#include<stdio>';
   return (
     <div className="executor__page">
       <Navbar />
@@ -19,6 +22,16 @@ function CodeExecution() {
         <section className="executor__code">
           <div className="code__header">
             <div className="code__heading">Code.cpp</div>
+
+{/* <select className="code_language" name="languages" id="languages"> */}
+<select onChange={(e => {
+                  console.log('this is on change', e);
+                  setSelectedLanguage(e.target.value)})}>
+  <option value="C++">C++</option>
+  <option value="C">C</option>
+  <option value="Java">Java</option>
+  <option value="Python">Python</option>
+</select>
             <button className="code__run" onClick={runCode}>
               Run
             </button>
@@ -29,7 +42,12 @@ function CodeExecution() {
             onChange={(e) => {
               setCode(e.target.value);
             }}
-          ></textarea>
+          >
+            
+          </textarea>
+              <SyntaxHighlighter language="cpp" style={docco} showLineNumbers="true" >
+      {code}
+    </SyntaxHighlighter>
         </section>
         <section className="executor__io">
           <div className="executor__input">
